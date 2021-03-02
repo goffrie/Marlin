16521e1b6d63f2c45e7de2db29bd59bfc18e1a5e
Geoffry Song <goffrie@gmail.com>
2021-03-02 12:15:55 -0800

    Attempt to make beeping less obnoxious

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1821,8 +1821,8 @@
 // Note: Test audio output with the G-Code:
 //  M300 S<frequency Hz> P<duration ms>
 //
-#define LCD_FEEDBACK_FREQUENCY_DURATION_MS 20
-#define LCD_FEEDBACK_FREQUENCY_HZ 1000
+#define LCD_FEEDBACK_FREQUENCY_DURATION_MS 5
+#define LCD_FEEDBACK_FREQUENCY_HZ 500
 
 //=============================================================================
 //======================== LCD / Controller Selection =========================
